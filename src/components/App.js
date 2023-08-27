75b946cb27d7db89deff4c218ef1014b895a6f34
saik12345 <111682963+saik12345@users.noreply.github.com>
2023-08-28 00:40:12 +0530

    Add files via upload
    necessary files added .

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+// app component
+export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleClear() {
+    const confirmed = window.confirm("sure ??");
+    if (confirmed) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClear={handleClear}
+      />
+
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/components/Stats.js
@@ -0,0 +1,17 @@
+//Stats Component
+export default function Stats({ items }) {
+  const numItems = items.length;
+  const numItemsPacked = items.filter((item) => item.packed).length;
+  // console.log(numItems, numItemsPacked, percentage);
+  const percentage = Math.round((numItemsPacked / numItems) * 100);
+  console.log(numItems, numItemsPacked, percentage);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything ready to go !! 🚗🚗🚗"
+          : `💼 You have ${numItems} items on your list, and you already packed ${numItemsPacked} items. ${percentage}%`}
+      </em>
+    </footer>
+  );
+}
