b3c42a78361cd9c8974ca8053e1b0fd0f8fbab30
saik12345 <111682963+saik12345@users.noreply.github.com>
2023-09-06 21:58:13 +0530

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -10,7 +10,7 @@ export default function Stats({ items }) {
       <em>
         {percentage === 100
           ? "You got everything ready to go !! 🚗🚗🚗"
-          : `💼 You have ${numItems} items on your list, and you already packed ${numItemsPacked} items. ${isNaN?0:percentage}%`}
+          : `💼 You have ${numItems} items on your list, and you already packed ${numItemsPacked} items. ${isNaN(percentage)?0:percentage}%`}
       </em>
     </footer>
   );
